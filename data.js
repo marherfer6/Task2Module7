1da30a3c301274a89cf7722bdcebc4f3f2e73bce
Marta <martahdz95@gmail.com>
2018-02-26 11:28:23 +0100

    New data

---
+++ data.js
@@ -0,0 +1,14 @@
+var totalSales = [
+    { month: new Date(2016,1, 01), sales: 9000 },
+    { month: new Date(2016,2, 01), sales: 8500 },
+    { month: new Date(2016,3, 01), sales: 5500 },
+    { month: new Date(2016,4, 01), sales: 6500 },
+    { month: new Date(2016,5, 01), sales: 7500 },
+    { month: new Date(2016,6, 01), sales: 8500 },
+    { month: new Date(2016,7, 01), sales: 5500 },
+    { month: new Date(2016,8, 01), sales: 6500 },
+    { month: new Date(2016,9, 01), sales: 7500 },
+    { month: new Date(2016,10, 01), sales: 6500 },
+    { month: new Date(2016,11, 01), sales: 5400 },
+    //{ month: new Date(2016,12, 01), sales: 3500 },
+    ];
---
+++ main.js
@@ -0,0 +1,119 @@
+// Let's start using ES6
+// And let's organize the code following clean code concepts
+// Later one we will complete a version using imports + webpack
+
+// Isolated data array to a different file
+
+let margin = null,
+    width = null,
+    height = null;
+
+let svg = null;
+let x, y = null; // scales
+
+setupCanvasSize();
+appendSvg("body");
+setupXScale();
+setupYScale();
+appendXAxis();
+appendYAxis();
+appendLineCharts();
+
+
+
+// 1. let's start by selecting the SVG Node
+function setupCanvasSize() {
+  margin = {top: 20, left: 80, bottom: 20, right: 30};
+  width = 960 - margin.left - margin.right;
+  height = 520 - margin.top - margin.bottom;
+}
+
+function appendSvg(domElement) {
+  svg = d3.select(domElement).append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform",`translate(${margin.left}, ${margin.top})`);
+
+}
+
+// Now on the X axis we want to map totalSales values to
+// pixels
+// in this case we map the canvas range 0..350, to 0...maxSales
+// domain == data (data from 0 to maxSales) boundaries
+function setupXScale()
+{
+
+  x = d3.scaleTime()
+      .range([0, width])
+      .domain(d3.extent(totalSales, function(d) { return d.month}));
+}
+
+// Now we don't have a linear range of values, we have a discrete
+// range of values (one per product)
+// Here we are generating an array of product names
+function setupYScale()
+{
+  var maxSales = d3.max(totalSales, function(d, i) {
+    return d.sales;
+  });
+
+  y = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, maxSales]);
+
+}
+
+function appendXAxis() {
+  // Add the X Axis
+  svg.append("g")
+    .attr("transform",`translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+}
+
+function appendYAxis() {
+  // Add the Y Axis
+  svg.append("g")
+  .call(d3.axisLeft(y));
+}
+
+// Define the div for the tooltip
+var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+function appendLineCharts()
+{
+  // define the line
+  var valueline = d3.line()
+                    .x(function(d) { return x(d.month); })
+                    .y(function(d) { return y(d.sales); });
+
+  // Add the valueline path.
+  svg.append("path")
+  .data([totalSales])
+  .attr("class", "line")
+  .attr("d", valueline);
+
+  svg.selectAll("dot")
+    .data(totalSales)
+    .enter().append("circle")
+    .attr("r", 3.5)
+    .attr("cx", function(d) { return setupYScale(d.month); })
+    .attr("cy", function(d) { return setupYScale(d.sales); })
+    .on("mouseover", function(d) {		
+        div.transition()		
+            .duration(200)		
+            .style("opacity", .9);		
+        div	.html((d.month) + "<br/>"  + d.sales)	
+            //.style("left", (d3.event.pageX) + "px")		
+            //.style("top", (d3.event.pageY - 28) + "px");	
+        })					
+    .on("mouseout", function(d) {		
+        div.transition()		
+            .duration(500)		
+            .style("opacity", 0);	
+    });
+
+}
+
